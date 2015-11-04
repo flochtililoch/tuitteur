9ddeb6c77a5f12dbc6636a7ca840a4142296710f
Florent Bonomo <florent@groupon.com>
2015-11-03 18:47:30 -0800

    Initial Commit

---
+++ Tuitteur/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Tuitteur
+//
+//  Created by Florent Bonomo on 11/3/15.
+//  Copyright © 2015 flochtililoch. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Tuitteur/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Tuitteur
+//
+//  Created by Florent Bonomo on 11/3/15.
+//  Copyright © 2015 flochtililoch. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
